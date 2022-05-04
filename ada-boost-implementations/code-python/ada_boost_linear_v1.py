BUGFIX: margin calculation for classifiers is added

ada-boost-implementations/code-python/ada_boost_linear_v1.py
@@ -8,7 +8,6 @@ from decision_stump_continuous import DecisionStumpContinuous as stump
 class AdaBoostLinear_v1:
     
     def __init__(self, tolerance=1e-10):
-        #self.n_estimators = -1
         self.tolerance = tolerance
         self.signs = (-1, 1)
         self.ensemble_coeffs = []
@@ -19,7 +18,6 @@ class AdaBoostLinear_v1:
     def fit(self, X, y, allow_nonseparable=False, trace=False):
         self.ensemble_coeffs = []
         self.ensemble_thresholds = []
-        #time_start = datetime.now()
         self.sample_size, features_count = X.shape[0], X.shape[1]
         n_estimators = 2*self.sample_size * features_count
         params = defaultdict(list) if trace else None
@@ -41,8 +39,6 @@ class AdaBoostLinear_v1:
                         sign, threshold = self.signs[sign_number], tmp_thresholds[classifier_pos] #X[ind[order_number, feature_number], feature_number]
                         h_k = stump.get_classification_scalar(X[sample_number, feature_number], threshold, sign) 
                         #sign if order[sample_number, feature_number] > order_number else -sign
-                        #a_ub[sample_number, feature_step*feature_number+order_step*order_number+sign_number] \
-                        #    = -h_k * y[sample_number]
                         a_ub[sample_number, classifier_pos] = -h_k * y[sample_number]
 
         b_ub = [0]*self.sample_size
@@ -73,13 +69,7 @@ class AdaBoostLinear_v1:
 
         self.ensemble_coeffs = coeffs[:-1]
         self.margin = coeffs[-1]
-        '''
-        tmp = np.take_along_axis(X, ind, axis=0)
-        tmp = np.repeat(tmp, repeats=2, axis=0)
-        self.ensemble_thresholds = tmp.flatten('F')
-        '''
         self.ensemble_thresholds = tmp_thresholds
-        #self.ensemble_signs = [-1, +1]*n_estimators
         if coeffs[-1] < 0:
             message += " Training set is nonseparable."
         return True, message, params
@@ -88,14 +78,6 @@ class AdaBoostLinear_v1:
         return self.margin
 
     def predict(self, X):
-        '''
-        sample_size_step, features_count, n_estimators = self.sample_size * 2, X.shape[1], len(self.ensemble_coeffs)
-        buffer = np.zeros(X.shape[0])
-        for coeff, number, threshold, sign in \
-                zip(self.ensemble_coeffs, range(n_estimators), self.ensemble_thresholds, self.signs*(int(n_estimators/2))):
-            feature_number = number // sample_size_step
-            buffer += coeff*np.array(stump.get_classification(X, feature_number, threshold, sign))
-        '''
         return np.sign(self.predict_raw(X))
 
     def predict_raw(self, X):
@@ -143,9 +125,7 @@ if __name__ == '__main__':
 
     clf = AdaBoostLinear_v1()
     result, message, log = clf.fit(X_train, y_train, trace=True)
+    y_pred = clf.predict(X_train)
+    assert (y_train==y_pred).all(), 'Wrong answer'
     print(result, message)
     print("Margin: ", clf.get_margin())
-    y_pred = clf.predict(X_train)
-    print(y_pred, y_train)
-#    print(X_train[0:2])
-#    print(clf.print_ensemble())

ada-boost-implementations/code-python/ada_boost_standard_v1.py
@@ -18,7 +18,6 @@ class AdaBoostStandardClassifier_v1:
         sample_size = len(y)
         d_t = sample_weigh if sample_weigh is not None else np.full(sample_size, 1/sample_size)
         history = defaultdict(list) if trace else None
-        ##d_t1 =  None
         for _ in range(self.n_estimators):
             minimal_error, decision_stump = self.get_decision_stump(X, y, d_t)
             if minimal_error >= 0.5:
@@ -28,20 +27,11 @@ class AdaBoostStandardClassifier_v1:
             self.log(trace, history, minimal_error, d_t, time_start)
             if minimal_error == 0:
                 return 'error_free_classifier_found', history
-            #d_t = np.dot(d_t, np.exp(-1*alpha_t*np.dot(y, decision_stump.classify(X))))
             d_t = np.multiply(d_t, np.exp(-alpha_t * np.multiply(y, decision_stump.classify(X))))
             d_t = d_t/(np.sum(d_t)+self.tolerance)
             
         return 'iterations_exceeded', history
-    
-    def predict(self, X):
-        sample_size = X.shape[0]
-        buffer = np.zeros(sample_size)
-        for elm in self.ensemble:
-            buffer += elm[0]*np.array(elm[1].classify(X))
-            
-        return np.sign(buffer)
-    
+
     def get_decision_stump(self, X, y, d_t):
         features_count = X.shape[1]
         minimal_error, minimal_feature, minimal_sign, minimal_threshold \
@@ -64,6 +54,22 @@ class AdaBoostStandardClassifier_v1:
             history['error'].append(minimal_error)
             history['d_t'].append(d_t)
 
+    def get_esemble_result(self, X):
+        sample_size = X.shape[0]
+        buffer = np.zeros(sample_size)
+        for elm in self.ensemble:
+            buffer += elm[0]*np.array(elm[1].classify(X))
+
+        return buffer
+
+    def predict(self, X):
+        return np.sign(self.get_esemble_result(X))
+
+    def get_margin_l1(self, X):
+        buffer = self.get_esemble_result(X)
+        alpha_modulo = sum([abs(elm[0]) for elm in self.ensemble]) + self.tolerance
+        return min(abs(buffer))/alpha_modulo
+
     def print_ensemble(self):
         value = ""
         for elm in self.ensemble:
@@ -83,8 +89,6 @@ if __name__ == '__main__':
     clf = AdaBoostStandardClassifier_v1(n_estimators=100)
     result, history = clf.fit(X_train, y_train, trace=True)
     print(result)
-    print(history['error'])
-    print(history['d_t'])
     y_pred = clf.predict(X_train)
-    print(y_pred)
-    print(clf.print_ensemble())
+    assert (y_train==y_pred).all(), 'Wrong answer'
+    print("Margin L1:", clf.get_margin_l1(X_train))